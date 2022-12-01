39063e4833c51f1bf38737ecc9b1dccf7c94f473
Daniel Rubiano <danielrubianomeneses@gmail.com>
2022-11-30 22:24:30 -0500

    agregando todos los archivos a main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src import create_app
 from src.config import configuration
 
-app = create_app(configuration["production"])
+app = create_app(configuration["development"])
 
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port=80)
+    app.run()
