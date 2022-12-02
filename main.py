56005af350bb2b712af2956562cd40324786b09b
Daniel Rubiano <danielrubianomeneses@gmail.com>
2022-12-01 22:47:17 -0500

    production

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.config import configuration
 """ para probar la app en modo development cambie el valor de configuracion
 por development o testing"""
 
-app = create_app(configuration["development"])
+app = create_app(configuration["production"])
 
 
 if __name__ == '__main__':
